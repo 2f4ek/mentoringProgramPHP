612b0ed5b7745b95c6fe70121efb9dab28bc244d
O.Tereshchenko <o.tereshchenko@wolterskluwer.com>
2024-10-23 21:45:06 +0300

    Refactoring

--- public/index.php
+++ public/index.php
@@ -1,14 +1,3 @@
 <?php
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
-
-use App\Module2\AbstractFactory\Person;
-use App\Module2\AbstractFactory\StorageFactoryHandler;
-
-$factory = StorageFactoryHandler::create('fs');
-
-$repository = $factory->createPersonRepository();
-$repository->savePerson(new Person("Alice", 100));
-print_r($repository->readPeople());
-$foundPerson = $repository->readPerson("Alice");
-echo "Found: " . $foundPerson->getName() . " with IQ " . $foundPerson->getIq() . "\n";
